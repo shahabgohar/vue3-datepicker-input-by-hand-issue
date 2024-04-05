1cd692188b77d31ca705ccd1ae9de0e94e457c78
shahabgohar <shahab.developer.work@gmail.com>
2024-04-05 09:51:39 +0500

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import VueDatePicker, { type VueDatePickerProps } from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import { ref } from 'vue'
+const test = ref()
+</script>
+
+<template>
+  <main>
+    <VueDatePicker
+      v-model="test"
+      :autoApply="true"
+      range
+      :partialFlow="true"
+      text-input
+    ></VueDatePicker>
+  </main>
+</template>
